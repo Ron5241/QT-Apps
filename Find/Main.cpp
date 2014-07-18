c1b48635d683395204237312db364fcea4212e47
Ronnie.P.Thomas <ronnietom@gmail.com>
2014-07-18 18:33:24 +0530

    Completed the Find Dialog

---
+++ Find/Main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+
+#include "FindDialog.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	FindDialog *dialog = new FindDialog;
+	dialog->show();
+	return app.exec();
+}
